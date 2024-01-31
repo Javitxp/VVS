40f5d34ecfe9dcc0e1eb3dbc89e50e67b6096335
sotomayor138070 <144667075+sotomayor138070@users.noreply.github.com>
2024-01-31 12:27:37 +0000

    Endpoint con id funcional

--- entrega1/analytics/users/index.php
+++ entrega1/analytics/users/index.php
@@ -1,3 +1,31 @@
 <?php
-    echo $_GET["id"];
+
+// URL de la API de Twitch
+$url = 'https://api.twitch.tv/helix/users?id='.$_GET["id"];
+
+// Encabezados de la solicitud
+$headers = array(
+    'Authorization: Bearer m8n110x82us492oc94ciqwx97iuo3t',
+    'Client-Id: rxbua83lt6p4yqdig92dvsoicmdi87'
+);
+
+$curl = curl_init();
+
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_HTTPGET, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($curl);
+
+if ($response === false) {
+    $error = curl_error($curl);
+    echo "Error al realizar la solicitud: " . $error;
+} else {
+    header("Content-Type: application/json");
+    echo $response;  
+}
+
+curl_close($curl);
+
 ?>
