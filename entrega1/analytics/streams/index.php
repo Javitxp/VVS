fec9f36a88735e3409b8af1cb626ed0ec58a8a69
OriolPatxangas <126689003+OriolPatxangas@users.noreply.github.com>
2024-02-05 12:31:35 +0100

    Add files via upload

---
+++ entrega1/analytics/streams/index.php
@@ -0,0 +1,49 @@
+<?php
+
+    $url = 'https://api.twitch.tv/helix/streams';
+
+    $headers = array(
+        'Authorization: Bearer m8n110x82us492oc94ciqwx97iuo3t',
+        'Client-Id: rxbua83lt6p4yqdig92dvsoicmdi87'
+    );
+
+    $curl = curl_init();
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPGET, true);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($curl);
+
+    if ($response === false) {
+        $error = curl_error($curl);
+        echo "Error al realizar la solicitud: " . $error;
+    } else {
+         // Decodificar la respuesta JSON
+        $streams_data = json_decode($response, true);
+
+        // Verificar si hay datos disponibles
+        if (isset($streams_data['data'])) {
+            // Obtener la información necesaria (nombre del usuario y título del stream)
+            $stream_info = array();
+            foreach ($streams_data['data'] as $stream) {
+                $stream_info[] = array(
+                    'user_name' => $stream['user_name'],
+                    'title' => $stream['title']
+                );
+            }
+
+            // Imprimir la información en formato JSON
+            header("Content-Type: application/json");
+            $obj = json_decode($stream_info,true);
+            $array = $obj['data'];
+            $json = json_encode($array[0]);
+            echo $json;
+        } else {
+            echo "No se encontraron datos de streams.";
+        }  
+    }
+
+    curl_close($curl);
+?>
