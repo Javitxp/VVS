f21cb70673ccf2f3e2eb46b00fa8ae404ef8efca
sotomayor138070 <144667075+sotomayor138070@users.noreply.github.com>
2024-02-19 12:26:58 +0000

    Añadida funcion para obtener los top 3 games

--- entrega2/analytics/topsofthetops/index.php
+++ entrega2/analytics/topsofthetops/index.php
@@ -1,5 +1,36 @@
 <?php
 
+    function getTop3Games() {
+        //Configurar url y autorizacion y client-id en header
+        $url = 'https://api.twitch.tv/helix/games/top?first=3';
+
+        $headers = array(
+            'Authorization: Bearer m8n110x82us492oc94ciqwx97iuo3t',
+            'Client-Id: rxbua83lt6p4yqdig92dvsoicmdi87'
+        );
+
+        //Configurar curl
+        $curl = curl_init();
+
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPGET, true);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curl);
+
+        if ($response === false) {
+            //$error = curl_error($curl);
+            //echo "Error al realizar la solicitud: " . $error;
+            return null;
+        } else {
+            $obj = json_decode($response, true);
+            $top3_games = $obj["data"];
+            return $top3_games;        
+        }
+        curl_close($curl);
+    }
+
     // Verificar si se recibió una solicitud GET
     if ($_SERVER['REQUEST_METHOD'] != 'GET') {
         http_response_code(405);
@@ -7,35 +38,18 @@
         exit(-1);
     }
 
-    //Configurar url y autorizacion y client-id en header
-    $url = 'https://api.twitch.tv/helix/games/top?first=3';
-
-    $headers = array(
-        'Authorization: Bearer m8n110x82us492oc94ciqwx97iuo3t',
-        'Client-Id: rxbua83lt6p4yqdig92dvsoicmdi87'
-    );
-
-    //Configurar curl
-    $curl = curl_init();
-
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_HTTPGET, true);
-    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-    $response = curl_exec($curl);
-
-    if ($response === false) {
-        $error = curl_error($curl);
-        echo "Error al realizar la solicitud: " . $error;
-    } else {
-        $obj = json_decode($response, true);
-        $array = $obj["data"];
-        $json = json_encode($array);
-        header("Content-Type: application/json");
-        echo $json;
-    
+    $top3_games = getTop3Games();
+    if($top3_games === null){
+        echo "Error al obtener top 3 juegos";
+        exit(-1);
     }
+
+    foreach ($top3_games as $game) {
+        $id = $game["id"];
+        echo $id;
+    }
+    //$json = json_encode($top3_games);
+    //header("Content-Type: application/json");
+    //echo $json;
     
-    curl_close($curl);
 ?>
