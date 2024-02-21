904b43c2355aa3c0edb23391181cf9089021633c
OriolPatxangas <126689003+OriolPatxangas@users.noreply.github.com>
2024-02-21 12:57:32 +0100

    Funciones para database añadidas

---
+++ entrega2/utils/database.php
@@ -0,0 +1,108 @@
+<?php
+    function connectToDB(){
+        $servername = "localhost";
+        $username = "id21862115_los_parseros";
+        $password = "P4rs3r0s*";
+        $database = "id21862115_twitch";
+
+        // Crear conexión
+        $conn = new mysqli($servername, $username, $password, $database);
+
+        // Verificar la conexión
+        if ($conn->connect_error) {
+            die("Conexión fallida: " . $conn->connect_error);
+        }
+
+        return $conn    
+    }
+
+    function closeConnectionDB($conn){
+        $conn->close();
+    }
+
+    function closeConnectionAndExitDB($conn){
+        $conn->close();
+        exit(-1);
+    }
+
+    function getTop3FromDB($conn){
+        $sql = "select * from Twitch_Entrega2;"
+        $result = $conn->query($sql)
+        $data = array();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()){
+                $data[] = $row;
+            }
+        }
+        return $data;
+    }
+
+    function setNew40GamesInDB($array, $conn){
+        //Primero eliminamos los ultimos 40 registros para meter los del siguiente juego
+        $sql = "DELETE FROM Prueba";
+
+        // Ejecutar la sentencia SQL
+        if ($conn->query($sql) === TRUE) {
+            foreach($array as $entry){
+                $sql = "INSERT INTO Prueba (title,user,views,duracion,fecha_creacion) VALUES ('".$entry["title"]."','".$entry["user_name"]."',".$entry["view_count"].",'".$entry["duration"]."','".$entry["created_at"]."')"
+                if ($conn->query($sql) === TRUE) {
+                    error_log("Registro anadido",0)
+                }else{
+                    echo "Error al introducir registros: " . $conn->error;
+                    closeConnectionAndExitDB();
+                }
+            }
+        } else {
+            echo "Error al eliminar registros: " . $conn->error;
+            closeConnectionAndExitDB();
+        }
+    }
+
+    function getVideosFromUserFromDB($user, $conn){
+        $sql = "SELECT user, COUNT(*) AS total_videos
+        FROM Prueba
+        WHERE user = '$user'
+        GROUP BY user;"
+        $result = $conn->query($sql)
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc()
+            return $row["total_videos"]
+        }else{
+            return -1;
+        }
+    }
+
+    function getSumViewsFromUserFromDB($user, $conn){
+        $sql = "SELECT user, SUM(views) AS total_views
+        FROM Prueba
+        WHERE user = '$user'
+        GROUP BY user;"
+        $result = $conn->query($sql)
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc()
+            return $row["total_views"]
+        }else{
+            return -1;
+        }
+    }
+
+    function getSumViewsFromUserFromDB($user, $conn){
+        $sql = "SELECT user, title, max(views) AS views_max, duracion, fecha_creacion
+        FROM Prueba
+        WHERE user = '$user'
+        GROUP BY user;"
+        $result = $conn->query($sql)
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc()
+            $data = array(
+                "most_viewed_title" => $row["title"],
+                "most_viewed_views" => $row["views_max"],
+                "most_viewed_duration" => $row["duracion"],
+                "most_viewed_created_at" => $row["fecha_creacion"];
+            );
+            return $row["total_views"]
+        }else{
+            return -1;
+        }
+    }
+?>
