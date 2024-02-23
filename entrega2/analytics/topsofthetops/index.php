a28689ebf3713603fac75a6a2aac864766c78855
eli <elzr.sr@gmail.com>
2024-02-23 14:05:46 +0100

    Función "Todo en Uno" getAndInsertTopOfTheTops

--- entrega2/analytics/topsofthetops/index.php
+++ entrega2/analytics/topsofthetops/index.php
@@ -1,57 +1,57 @@
 <?php
 
-    include "../../utils/api.php";
-    include "../../utils/database.php";
+    include '../../utils/api.php';
+    include '../../utils/database.php';
 
-    // Verificar si se recibió una solicitud GET
+    function getAndInsertTopOfTheTops(){
+        $conn = connectToDB();
+        $top3_games = getTop3Games();
+        if ($top3_games === null) {
+            echo "Error al obtener top 3 juegos";
+            exit(-1);
+        }
+        $topsOfTheTops = [];
+        foreach ($top3_games as $game) {
+            $id = $game["id"];
+            $name = $game["name"];
+            $array = getTop40Videos($id);
+
+            $topUser = $array[0];
+            $user = $topUser["user_name"];
+            setNew40GamesInDB($array, $conn);
+
+            $totalVideos = getVideosFromUserFromDB($user, $conn);
+            $sum = getSumViewsFromUserFromDB($user, $conn);
+            $dataMostViewed = getMostViewedFromUserFromDB($user, $conn);
+
+            $allData = array(
+                "game_id" => $id,
+                "game_name" => $name,
+                "user_name" => $user,
+                "total_videos" => $totalVideos,
+                "total_views" => $sum,
+                "most_viewed_title" => $dataMostViewed["most_viewed_title"],
+                "most_viewed_views" => $dataMostViewed["most_viewed_views"],
+                "most_viewed_duration" => $dataMostViewed["most_viewed_duration"],
+                "most_viewed_created_at" => $dataMostViewed["most_viewed_created_at"]
+            );
+
+            $topsOfTheTops[] = $allData;
+        }
+
+        insertNewTopOfTheTops($conn, json_encode($topsOfTheTops));
+        closeConnectionDB($conn);
+        return json_encode($topsOfTheTops);
+    }
+
     if ($_SERVER['REQUEST_METHOD'] != 'GET') {
         http_response_code(405);
         echo "Only GET requests";
         exit(-1);
     }
 
-    $conn = connectToDB();
-
-    $top3_games = getTop3Games();
-    if($top3_games === null){
-        echo "Error al obtener top 3 juegos";
-        exit(-1);
-    }
-
-    $topsOfTheTops = array();
-    foreach ($top3_games as $game) {
-        $id = $game["id"];
-        $name = $game["name"];
-        $array = getTop40Videos($id);
-
-        $topUser = $array[0];
-        $user = $topUser["user_name"];
-        setNew40GamesInDB($array,$conn);
-
-        $totalVideos = getVideosFromUserFromDB($user, $conn);
-        $sum = getSumViewsFromUserFromDB($user, $conn);
-        $dataMostViewed = getMostViewedFromUserFromDB($user, $conn);
-
-        $allData = array(
-            "game_id" => $id,
-            "game_name" => $name,
-            "user_name" => $user,
-            "total_videos" => $totalVideos,
-            "total_views" => $sum,
-            "most_viewed_title" => $dataMostViewed["most_viewed_title"],
-            "most_viewed_views" => $dataMostViewed["most_viewed_views"],
-            "most_viewed_duration" => $dataMostViewed["most_viewed_duration"],
-            "most_viewed_created_at" => $dataMostViewed["most_viewed_created_at"]
-        );
-
-        $topsOfTheTops[] = $allData;
-
-        insertNewTopOfTheTops($conn, json_encode($allData));
-    }
-    $json = json_encode($topsOfTheTops);
+    $json = getAndInsertTopOfTheTops();
     header("Content-Type: application/json");
     echo $json;
-
-    closeConnectionDB($conn);
         
 ?>
