61267bbacc99157e1ae247da6b25836c6fc0522b
sotomayor138070 <144667075+sotomayor138070@users.noreply.github.com>
2024-02-28 11:55:08 +0000

    Primer commit sol1

--- entrega2/analytics/topsofthetops/index.php
+++ entrega2/analytics/topsofthetops/index.php
@@ -8,11 +8,46 @@
         exit(-1);
     }
 
-    $json = isset($_GET["since"]) ? getSinceTopOfTheTops($_GET["since"]) : getLast10MinTopOfTheTops();
+    /*$json = isset($_GET["since"]) ? getSinceTopOfTheTops($_GET["since"]) : getLast10MinTopOfTheTops();
 
     if($json === null){
         $json = getAndInsertTopOfTheTops();
     }
+    */
+
+    $conn = connectToDB();
+        $top3_games = getTop3Games();
+        if ($top3_games === null) {
+            echo "Error al obtener top 3 juegos";
+            exit(-1);
+        }
+        $topsOfTheTops = [];
+        foreach ($top3_games as $game) {
+            $id = $game["id"];
+            $name = $game["name"];
+
+            // CHECK SI ESTA EN PRESENTAR CON ID
+            $result = checkGameId($id);
+
+            // ESTA EN PRESENTAR
+            if($result){
+                // REVISAR TIMESTAMP
+                // SI HAY SINCE
+                    // COMPARAR SINCE CON TIMESTAMP
+                // NO HAY SINCE
+                    // COMPARAR 10MIN CON TIMESTAMP  
+                $json = isset($_GET["since"]) ? getSince($_GET["since"],$id) : getLast10($id);
+                if($json === null){
+                    //$json = getAndInsertGame($id);
+                    $json = updateGame($id);
+                }
+            }
+            // NO ESTA EN PRESENTAR
+            else{
+                // OBTENER TODA LA DATA DEL JUEGO
+                $json = getAndInsertGame($id);
+            }
+        }
 
     header("Content-Type: application/json");
     echo $json;
