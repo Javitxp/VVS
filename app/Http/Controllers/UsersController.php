4950d4fbb851db13d58b5093fae0f6731d5277bd
Javitxp <pernaut.134063@e.unavarra.es>
2024-05-04 13:01:50 +0200

    [5] - Adaptado endpoint Users a Laravel Controller

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,15 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class UsersController extends Controller
 {
-    function __construct() {}
+    public function __construct()
+    {
+    }
     /**
      * Handle the incoming request
      */
-    public function __invoke(Request $request){
+    public function getStreams(Request $request): JsonResponse
+    {
+        // Verificar si se recibió una solicitud GET
+        if (!$request->isMethod('get')) {
+            return response()->json(['message' => 'Only GET requests'], 405);
+        }
 
+        // Verificar si se proporcionó el parámetro id
+        if (!$request->has('id')) {
+            return response()->json(['message' => 'Parameter id required'], 400);
+        }
+
+        // Configurar url y autorización y client-id en el encabezado
+        $url = 'https://api.twitch.tv/helix/users?id=' . $request->input('id');
+
+        $headers = [
+            'Authorization' => 'Bearer sga6x1z0df1pq1mskps75mjmhwa3p4',
+            'Client-Id' => 'rxbua83lt6p4yqdig92dvsoicmdi87'
+        ];
+
+        // Realizar la solicitud HTTP
+        $response = Http::withHeaders($headers)->get($url);
+
+        // Verificar si la solicitud fue exitosa
+        if ($response->successful()) {
+            $user_data = $response->json();
+
+            // Verificar si hay datos disponibles
+            if (isset($user_data['data']) && count($user_data['data']) > 0) {
+                // Devolver la información del usuario en formato JSON
+                return response()->json($user_data['data'][0]);
+            } else {
+                return response()->json(['message' => 'There is no user with id: ' . $request->input('id')], 404);
+            }
+        } else {
+            return response()->json(['message' => 'Error al realizar la solicitud: ' . $response->status()], 500);
+        }
     }
 }
