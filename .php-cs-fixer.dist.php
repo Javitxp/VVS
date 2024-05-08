52a1e71e633419180aad5cc7e91d2205971006e5
UsamaBoukhebache <uboukhebache@gmail.com>
2024-05-08 20:35:57 +0200

    [#11] - Corregidas reglas fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,14 +4,12 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $finder = Finder::create()
-    ->in(__DIR__ . '/app') // Buscar en el directorio 'app' en la raíz de tu proyecto
+    ->in(__DIR__ . '/app')
     ->exclude('vendor');
 
 $config = new Config();
 $config->setRules([
-    'class_name' => [
-        'format' => 'camel_case', // Esto especifica que los nombres de las clases deben estar en CamelCase
-    ],
+    '@PSR12' => true,
 ]);
 $config->setFinder($finder);
 
