275a7ed64feb544013e4d86952d86b7e8a959134
eli <sotomayor.138070@e.unavarra.es>
2024-05-09 17:58:29 +0200

    [#5] - Devolviendo campo data en StreamsDataProvider

--- app/Services/StreamsDataProvider.php
+++ app/Services/StreamsDataProvider.php
@@ -7,13 +7,16 @@ use App\Infrastructure\Clients\ApiClient;
 class StreamsDataProvider
 {
     private ApiClient $apiClient;
-    public function __construct(ApiClient $apiClient){
+    public function __construct(ApiClient $apiClient)
+    {
         $this->apiClient = $apiClient;
     }
-    public function execute(){
+    public function execute()
+    {
         $url = 'https://api.twitch.tv/helix/streams';
         $headers = array('Authorization: Bearer '.$this->apiClient->getToken());
         $response = $this->apiClient->makeCurlCall($url, $headers);
-        return $response;
+        $data = json_decode($response, true)['data'];
+        return $data;
     }
 }
