eef44c96c2010ec3cbde96f14f5fd46807a19d23
Javitxp <javier.pernaut@gmail.com>
2024-05-13 13:24:01 +0200

    [#18] - Cambiada ruta a endpoint streamers en tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -30,7 +30,7 @@ class ExampleTest extends TestCase
      */
     public function analyticsUsersReturns500WithoutId(): void
     {
-        $response = $this->get('/analytics/users');
+        $response = $this->get('/analytics/streamers');
 
         $response->assertStatus(500);
         $response->assertJson(['message' => 'Parameter id required']);
@@ -41,7 +41,7 @@ class ExampleTest extends TestCase
      */
     public function analyticsUsersReturns500WithNonNumericId(): void
     {
-        $response = $this->get('/analytics/users?id=abcde');
+        $response = $this->get('/analytics/streamers?id=abcde');
 
         $response->assertStatus(500);
         $response->assertJson(['message' => 'Parameter id must be a number']);
@@ -74,7 +74,7 @@ class ExampleTest extends TestCase
         $userDataProvider->expects('getUserData')->with($getExpectedToken)->andReturn($getUsersExpectedResponse);
         $userDataProvider->expects('setUserId')->withArgs(["20"]);
 
-        $response = $this->get('/analytics/users?id=20');
+        $response = $this->get('/analytics/streamers?id=20');
 
         $response->assertStatus(200);
         $response->assertContent('"{\"user_id\":\"id\",\"user_name\":\"user_name\"}"');
@@ -156,7 +156,7 @@ class ExampleTest extends TestCase
         $userDataManagerMock->shouldReceive('getUserData')
             ->andThrow(new Exception("No se puede establecer conexión con Twitch en este momento", ErrorCodes::TOKEN_500));
 
-        $response = $this->get('/analytics/users?id=69');
+        $response = $this->get('/analytics/streamers?id=69');
 
         $response->assertStatus(503);
         $response->assertJson(['error' => "No se puede establecer conexión con Twitch en este momento"]);
@@ -173,7 +173,7 @@ class ExampleTest extends TestCase
         $userDataManagerMock->shouldReceive('getUserData')
             ->andThrow(new Exception("No se pueden devolver usuarios en este momento, inténtalo más tarde", ErrorCodes::USERS_500));
 
-        $response = $this->get('/analytics/users?id=69');
+        $response = $this->get('/analytics/streamers?id=69');
 
         $response->assertStatus(503);
         $response->assertJson(['error' => "No se pueden devolver usuarios en este momento, inténtalo más tarde"]);
