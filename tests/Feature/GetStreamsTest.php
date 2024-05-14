954137ffaafae766b79188af378bc9edfa7a9146
Oriol Leon <leon.132157@e.unavarra.es>
2024-05-14 18:16:15 +0200

    [#26] - Primer refactor de testing para streamers y streams

--- tests/Feature/GetStreamsTest.php
+++ tests/Feature/GetStreamsTest.php
@@ -1,6 +1,95 @@
 <?php
 
-class GetStreamsTest
-{
+namespace Tests\Feature;
 
+use App\Infrastructure\Clients\ApiClient;
+use App\Services\StreamsDataManager;
+use App\Services\StreamsDataProvider;
+use App\Services\TokenProvider;
+use App\Utilities\ErrorCodes;
+use Exception;
+use Mockery;
+use Tests\TestCase;
+
+class GetStreamsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function GetsStreams(): void
+    {
+        $streamsDataProvider = Mockery::mock(StreamsDataProvider::class);
+        $tokenProvider = Mockery::mock(TokenProvider::class);
+
+        $this->app
+            ->when(StreamsDataManager::class)
+            ->needs(StreamsDataProvider::class)
+            ->give(fn () => $streamsDataProvider);
+
+        $this->app
+            ->when(StreamsDataManager::class)
+            ->needs(TokenProvider::class)
+            ->give(fn () => $tokenProvider);
+
+        $getExpectedToken = 'token';
+        $tokenProvider->expects('getToken')->andReturn($getExpectedToken);
+        $getStreamsExpectedResponse = json_encode(['title' => 'title', 'user_name' => 'user_name']);
+        $streamsDataProvider->expects('execute')->with($getExpectedToken)->andReturn($getStreamsExpectedResponse);
+
+        $response = $this->get('/analytics/streams');
+
+        $response->assertStatus(200);
+        $response->assertContent('"{\"title\":\"title\",\"user_name\":\"user_name\"}"');
+    }
+    /**
+     * @test
+     */
+    public function GetsStreams2(): void
+    {
+        $apiClient = Mockery::mock(ApiClient::class);
+        $this->app
+            ->when(StreamsDataProvider::class)
+            ->needs(ApiClient::class)
+            ->give(fn () => $apiClient);
+
+        $getExpectedToken = 'token';
+        $apiClient->expects('getToken')->andReturn($getExpectedToken);
+        $getStreamsExpectedResponse = json_encode(['title' => 'title', 'user_name' => 'user_name']);
+        $apiClient->expects('makeCurlCall')->with('https://api.twitch.tv/helix/streams', [0 => 'Authorization: Bearer token'])->andReturn($getStreamsExpectedResponse);
+
+        $response = $this->get('/analytics/streams');
+
+        $response->assertStatus(200);
+        $response->assertContent('"{\"title\":\"title\",\"user_name\":\"user_name\"}"');
+    }
+
+    /**
+     * @test
+     */
+    public function ErrorIfFailInGettingToken(): void
+    {
+        $streamsDataManagerMock = $this->mock(StreamsDataManager::class);
+        $streamsDataManagerMock->shouldReceive('getStreams')
+            ->andThrow(new Exception("No se puede establecer conexión con Twitch en este momento", ErrorCodes::TOKEN_500));
+
+        $response = $this->get('/analytics/streams');
+
+        $response->assertStatus(503);
+        $response->assertJson(['error' => "No se puede establecer conexión con Twitch en este momento"]);
+    }
+
+    /**
+     * @test
+     */
+    public function ErrorIfFailInGettingStreams(): void
+    {
+        $streamsDataManagerMock = $this->mock(StreamsDataManager::class);
+        $streamsDataManagerMock->shouldReceive('getStreams')
+            ->andThrow(new Exception("No se pueden devolver streams en este momento, inténtalo más tarde", ErrorCodes::STREAMS_500));
+
+        $response = $this->get('/analytics/streams');
+
+        $response->assertStatus(503);
+        $response->assertJson(['error' => "No se pueden devolver streams en este momento, inténtalo más tarde"]);
+    }
 }
--- tests/Feature/GetUsersTest.php
+++ tests/Feature/GetUsersTest.php
@@ -1,6 +1,94 @@
 <?php
 
-class GetUsersTest
+namespace Tests\Feature;
+use App\Services\TokenProvider;
+use App\Services\UserDataManager;
+use App\Services\UserDataProvider;
+use App\Utilities\ErrorCodes;
+use Exception;
+use Mockery;
+use Tests\TestCase;
+class GetUsersTest extends TestCase
 {
+    /**
+     * @test
+     */
+    public function ErrorIfNoIdGivenInUsers(): void
+    {
+        $response = $this->get('/analytics/users');
 
+        $response->assertStatus(500);
+        $response->assertJson(['message' => 'Parameter id required']);
+    }
+
+    /**
+     * @test
+     */
+    public function ErrorIfNoNumericIdGivenInUsers(): void
+    {
+        $response = $this->get('/analytics/users?id=abcde');
+
+        $response->assertStatus(500);
+        $response->assertJson(['message' => 'Parameter id must be a number']);
+    }
+
+    /**
+     * @test
+     */
+    public function GetsUsers(): void
+    {
+        $userDataProvider = Mockery::mock(UserDataProvider::class);
+        $tokenProvider = Mockery::mock(TokenProvider::class);
+
+        $this->app
+            ->when(UserDataManager::class)
+            ->needs(UserDataProvider::class)
+            ->give(fn () => $userDataProvider);
+
+        $this->app
+            ->when(UserDataManager::class)
+            ->needs(TokenProvider::class)
+            ->give(fn () => $tokenProvider);
+
+        $getExpectedToken = 'token';
+
+        $getUsersExpectedResponse = json_encode(['user_id' => 'id', 'user_name' => 'user_name']);
+
+        $tokenProvider->expects('getToken')->andReturn($getExpectedToken);
+
+        $userDataProvider->expects('getUserData')->with($getExpectedToken, "20")->andReturn($getUsersExpectedResponse);
+
+        $response = $this->get('/analytics/users?id=20');
+
+        $response->assertStatus(200);
+        $response->assertContent('"{\"user_id\":\"id\",\"user_name\":\"user_name\"}"');
+    }
+    /**
+     * @test
+     */
+    public function ErrorWhenFailInGettingToken(): void
+    {
+        $userDataManagerMock = $this->mock(UserDataManager::class);
+        $userDataManagerMock->shouldReceive('getUserData')
+            ->andThrow(new Exception("No se puede establecer conexión con Twitch en este momento", ErrorCodes::TOKEN_500));
+
+        $response = $this->get('/analytics/users?id=69');
+
+        $response->assertStatus(503);
+        $response->assertJson(['error' => "No se puede establecer conexión con Twitch en este momento"]);
+    }
+    /**
+     * @test
+     */
+    public function ErrorWhenFailInGettingUsers(): void
+    {
+        $userDataManagerMock = $this->mock(UserDataManager::class);
+        $userDataManagerMock->shouldReceive('getUserData')
+            ->andThrow(new Exception("No se pueden devolver usuarios en este momento, inténtalo más tarde", ErrorCodes::USERS_500));
+
+        $response = $this->get('/analytics/users?id=69');
+
+        $response->assertStatus(503);
+        $response->assertJson(['error' => "No se pueden devolver usuarios en este momento, inténtalo más tarde"]);
+    }
 }
