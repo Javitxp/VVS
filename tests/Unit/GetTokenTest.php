fafc266871d6524ed518cc9fe7ea6dae44b66065
Oriol Leon <leon.132157@e.unavarra.es>
2024-05-30 18:15:11 +0200

    Test del token provider añadido a carpeta de tests unitarios

--- tests/Unit/GetTokenTest.php
+++ tests/Unit/GetTokenTest.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Services\TokenProvider;
+namespace Tests\Unit;
+
 use App\Infrastructure\Clients\ApiClient;
 use App\Infrastructure\Clients\DBClient;
+use App\Services\TokenProvider;
 use Mockery;
 use Tests\TestCase;
 
@@ -26,29 +28,29 @@ class GetTokenTest extends TestCase
      */
     public function testGetTokenFromDB()
     {
-        $expectedToken = "token_from_db";	
+        $expectedToken = "token_from_db";
 
         $this->dbClientMock->expects('getToken')
-                     ->andReturn('token_from_db');
+            ->andReturn('token_from_db');
 
         $token = $this->tokenProvider->getToken();
 
         $this->assertEquals($expectedToken, $token);
     }
-    
+
     /**
      * @test
      */
     public function testGetTokenFromAPI()
     {
-        $expectedToken = "token_from_api";	
+        $expectedToken = "token_from_api";
 
         $this->dbClientMock->expects('getToken')
-                     ->andReturn(null);
+            ->andReturn(null);
         $this->apiClientMock->expects('getToken')
-                      ->andReturn('token_from_api');
+            ->andReturn('token_from_api');
         $this->dbClientMock->expects('replaceToken')
-                      ->andReturn(true);
+            ->andReturn(true);
 
         $token = $this->tokenProvider->getToken();
 
