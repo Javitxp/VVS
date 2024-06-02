d663735fd3921224ba6a72a5a10d37be7b921ea9
eli <sotomayor.138070@e.unavarra.es>
2024-06-02 17:37:14 +0200

    [#42] - Fix status code

--- app/Infrastructure/Controllers/UnfollowStreamerController.php
+++ app/Infrastructure/Controllers/UnfollowStreamerController.php
@@ -7,17 +7,13 @@ use App\Utilities\ErrorCodes;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use App\Services\UserDataManager;
-use App\Services\StreamerDataManager;
 
 class UnfollowStreamerController extends Controller
 {
     private UserDataManager $userDataManager;
-    private StreamerDataManager $streamerDataManager;
-
-    public function __construct(UserDataManager $userDataManager, StreamerDataManager $streamerDataManager)
+    public function __construct(UserDataManager $userDataManager)
     {
         $this->userDataManager = $userDataManager;
-        $this->streamerDataManager = $streamerDataManager;
     }
 
     /**
@@ -29,15 +25,13 @@ class UnfollowStreamerController extends Controller
             $userId = $request->input("userId");
             $streamerId = $request->input("streamerId");
 
-            // $this->streamerDataManager->getStreamerData($request->input("streamerId"));
-
             $user = $this->userDataManager->unfollowStreamer(
                 $userId,
                 $streamerId
             );
             return response()->json([
                 'message' => 'Dejaste de seguir a ' . $streamerId
-            ], 201);
+            ], 200);
 
         } catch (Exception $exception) {
             $message = match ($exception->getCode()) {
