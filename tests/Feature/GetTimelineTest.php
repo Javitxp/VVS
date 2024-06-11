d70c6530dc8a26a26113694acc972c71f82a2904
Oriol Leon <leon.132157@e.unavarra.es>
2024-06-11 11:22:17 +0200

    [#45] - extraido setup de GetTimeline

--- tests/Feature/GetTimelineTest.php
+++ tests/Feature/GetTimelineTest.php
@@ -5,163 +5,110 @@ use App\Services\UserDataProvider;
 use App\Utilities\ErrorCodes;
 use Tests\TestCase;
 use Exception;
+use Mockery;
 
 class GetTimelineTest extends TestCase
 {
+    protected UserDataProvider $userDataProviderMock;
+    protected TokenProvider $tokenProviderMock;
+    protected string $userId;
+    protected string $expectedToken;
+    protected array $expectedTimeline;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->userId = "1";
+        $this->expectedToken = 'token';
+        $this->expectedTimeline = [
+            [
+                "streamerId" => "streamer1",
+                "streamerName" => "Streamer 1",
+                "title" => "Stream 1",
+                "game" => "Game 1",
+                "viewerCount" => 100,
+                "startedAt" => "2024-05-10T12:00:00Z"
+            ],
+            [
+                "streamerId" => "streamer1",
+                "streamerName" => "Streamer 1",
+                "title" => "Stream 2",
+                "game" => "Game 2",
+                "viewerCount" => 100,
+                "startedAt" => "2024-05-10T12:00:00Z"
+            ],
+            [
+                "streamerId" => "streamer1",
+                "streamerName" => "Streamer 1",
+                "title" => "Stream 3",
+                "game" => "Game 3",
+                "viewerCount" => 100,
+                "startedAt" => "2024-05-10T12:00:00Z"
+            ],
+            [
+                "streamerId" => "streamer1",
+                "streamerName" => "Streamer 1",
+                "title" => "Stream 2",
+                "game" => "Game 2",
+                "viewerCount" => 100,
+                "startedAt" => "2024-05-10T12:00:00Z"
+            ],
+            [
+                "streamerId" => "streamer1",
+                "streamerName" => "Streamer 1",
+                "title" => "Stream 2",
+                "game" => "Game 2",
+                "viewerCount" => 100,
+                "startedAt" => "2024-05-10T12:00:00Z"
+            ],
+        ];
+        $this->userDataProviderMock = Mockery::mock(UserDataProvider::class);
+        $this->tokenProviderMock = Mockery::mock(TokenProvider::class);
+        $this->tokenProviderMock->expects('getToken')->andReturn($this->expectedToken);
+        $this->app->instance(UserDataProvider::class, $this->userDataProviderMock);
+        $this->app->instance(TokenProvider::class, $this->tokenProviderMock);
+    }
+
     /**
      * @test
      */
     public function GetsTimeline()
     {
-        $userId = "1";
-        $expectedToken = 'token';
-        $userDataProviderMock = Mockery::mock(UserDataProvider::class);
-        $tokenProviderMock = Mockery::mock(TokenProvider::class);
-        $tokenProviderMock->expects('getToken')
-            ->andReturn($expectedToken);
-        $userDataProviderMock->expects('getUserFollowedStreamersTimeline')
-            ->with($expectedToken, $userId)
-            ->andReturn([
-                0 =>
-                [
-                    "streamerId" => "streamer1",
-                    "streamerName" => "Streamer 1",
-                    "title" => "Stream 1",
-                    "game" => "Game 1",
-                    "viewerCount" => 100,
-                    "startedAt" => "2024-05-10T12:00:00Z"
-                ],
-                1 =>
-                    [
-                        "streamerId" => "streamer1",
-                        "streamerName" => "Streamer 1",
-                        "title" => "Stream 2",
-                        "game" => "Game 2",
-                        "viewerCount" => 100,
-                        "startedAt" => "2024-05-10T12:00:00Z"
-                    ],
-                2 =>
-                    [
-                        "streamerId" => "streamer1",
-                        "streamerName" => "Streamer 1",
-                        "title" => "Stream 3",
-                        "game" => "Game 3",
-                        "viewerCount" => 100,
-                        "startedAt" => "2024-05-10T12:00:00Z"
-                    ],
-                3 =>
-                    [
-                        "streamerId" => "streamer1",
-                        "streamerName" => "Streamer 1",
-                        "title" => "Stream 2",
-                        "game" => "Game 2",
-                        "viewerCount" => 100,
-                        "startedAt" => "2024-05-10T12:00:00Z"
-                    ],
-                4 =>
-                    [
-                        "streamerId" => "streamer1",
-                        "streamerName" => "Streamer 1",
-                        "title" => "Stream 2",
-                        "game" => "Game 2",
-                        "viewerCount" => 100,
-                        "startedAt" => "2024-05-10T12:00:00Z"
-                    ],
-            ]);
-        $this->app->instance(UserDataProvider::class, $userDataProviderMock);
-        $this->app->instance(TokenProvider::class, $tokenProviderMock);
+        $this->userDataProviderMock->expects('getUserFollowedStreamersTimeline')
+            ->with($this->expectedToken, $this->userId)
+            ->andReturn($this->expectedTimeline);
 
-        $response = $this->get('/analytics/timeline/'.$userId);
+        $response = $this->get('/analytics/timeline/'.$this->userId);
 
         $response->assertStatus(200);
-        $response->assertJson([
-            0 =>
-                [
-                    "streamerId" => "streamer1",
-                    "streamerName" => "Streamer 1",
-                    "title" => "Stream 1",
-                    "game" => "Game 1",
-                    "viewerCount" => 100,
-                    "startedAt" => "2024-05-10T12:00:00Z"
-                ],
-            1 =>
-                [
-                    "streamerId" => "streamer1",
-                    "streamerName" => "Streamer 1",
-                    "title" => "Stream 2",
-                    "game" => "Game 2",
-                    "viewerCount" => 100,
-                    "startedAt" => "2024-05-10T12:00:00Z"
-                ],
-            2 =>
-                [
-                    "streamerId" => "streamer1",
-                    "streamerName" => "Streamer 1",
-                    "title" => "Stream 3",
-                    "game" => "Game 3",
-                    "viewerCount" => 100,
-                    "startedAt" => "2024-05-10T12:00:00Z"
-                ],
-            3 =>
-                [
-                    "streamerId" => "streamer1",
-                    "streamerName" => "Streamer 1",
-                    "title" => "Stream 2",
-                    "game" => "Game 2",
-                    "viewerCount" => 100,
-                    "startedAt" => "2024-05-10T12:00:00Z"
-                ],
-            4 =>
-                [
-                    "streamerId" => "streamer1",
-                    "streamerName" => "Streamer 1",
-                    "title" => "Stream 2",
-                    "game" => "Game 2",
-                    "viewerCount" => 100,
-                    "startedAt" => "2024-05-10T12:00:00Z"
-                ],
-        ]);
+        $response->assertJson($this->expectedTimeline);
     }
+
     /**
      * @test
      */
     public function ReturnsFileNotFoundWhenCallingWithANonExistentUser()
     {
-        $userId = "1";
-        $expectedToken = 'token';
-        $userDataProviderMock = Mockery::mock(UserDataProvider::class);
-        $tokenProviderMock = Mockery::mock(TokenProvider::class);
-        $tokenProviderMock->expects('getToken')
-            ->andReturn($expectedToken);
-        $userDataProviderMock->expects('getUserFollowedStreamersTimeline')
-            ->with($expectedToken, $userId)
+        $this->userDataProviderMock->expects('getUserFollowedStreamersTimeline')
+            ->with($this->expectedToken, $this->userId)
             ->andThrow(new Exception("El usuario especificado no existe.", ErrorCodes::TIMELINE_404));
-        $this->app->instance(UserDataProvider::class, $userDataProviderMock);
-        $this->app->instance(TokenProvider::class, $tokenProviderMock);
 
-        $response = $this->get('/analytics/timeline/'.$userId);
+        $response = $this->get('/analytics/timeline/'.$this->userId);
 
         $response->assertStatus(404);
         $response->assertJson(['error' => 'El usuario especificado no existe.']);
     }
+
     /**
      * @test
      */
     public function ReturnsServerErrorWhenCallFails()
     {
-        $userId = "1";
-        $expectedToken = 'token';
-        $userDataProviderMock = Mockery::mock(UserDataProvider::class);
-        $tokenProviderMock = Mockery::mock(TokenProvider::class);
-        $tokenProviderMock->expects('getToken')
-            ->andReturn($expectedToken);
-        $userDataProviderMock->expects('getUserFollowedStreamersTimeline')
-            ->with($expectedToken, $userId)
+        $this->userDataProviderMock->expects('getUserFollowedStreamersTimeline')
+            ->with($this->expectedToken, $this->userId)
             ->andThrow(new Exception("Error al obtener los streams de Twitch.", ErrorCodes::TIMELINE_500));
-        $this->app->instance(UserDataProvider::class, $userDataProviderMock);
-        $this->app->instance(TokenProvider::class, $tokenProviderMock);
 
-        $response = $this->get('/analytics/timeline/'.$userId);
+        $response = $this->get('/analytics/timeline/'.$this->userId);
 
         $response->assertStatus(500);
         $response->assertJson(['error' => 'Error al obtener los streams de Twitch.']);
