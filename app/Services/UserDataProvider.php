07e7a4f5291663b96c3b3b75912cb47bd1d87e4b
Oriol Leon <leon.132157@e.unavarra.es>
2024-06-14 11:16:05 +0200

    [#50] - Separadas responsabilidades en UDP

--- app/Services/UserDataProvider.php
+++ app/Services/UserDataProvider.php
@@ -2,20 +2,19 @@
 
 namespace App\Services;
 
+use App\Infrastructure\Clients\ApiClient;
 use App\Infrastructure\Clients\DBClient;
-use App\Models\RegistredUser;
 use App\Utilities\ErrorCodes;
 use Exception;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\DB;
 
 class UserDataProvider
 {
     private DBClient $dbClient;
-    public function __construct(DBClient $dbClient)
+    private ApiClient $apiClient;
+    public function __construct(DBClient $dbClient, ApiClient $apiClient)
     {
         $this->dbClient = $dbClient;
+        $this->apiClient = $apiClient;
     }
 
     /**
@@ -23,20 +22,12 @@ class UserDataProvider
      */
     public function createUser($username, $password)
     {
-        // Verificar si el usuario ya existe
         if($this->dbClient->checkUsername($username)) {
             throw new Exception("El nombre de usuario ya está en uso.", ErrorCodes::USERS_409);
         }
-
         try {
-            $user = new RegistredUser();
-            $user->username = $username;
-            $user->password = Hash::make($password);
-            $user->followedStreamers = json_encode([]);
-            $this->dbClient->insertUser($username, Hash::make($password), json_encode([]));
-
-            return $user;
-        } catch (Exception $e) {
+            return $this->dbClient->insertUser($username, $password);
+        } catch (Exception $exception) {
             throw new Exception("Error al crear el usuario.", ErrorCodes::USERS_500);
         }
     }
@@ -47,7 +38,7 @@ class UserDataProvider
     {
         try {
             return $this->dbClient->getAllUsers();
-        } catch (Exception $e) {
+        } catch (Exception $exception) {
             throw new Exception("Error al obtener la lista de usuarios.", ErrorCodes::USERS_500);
         }
     }
@@ -57,35 +48,17 @@ class UserDataProvider
      */
     public function getUserFollowedStreamersTimeline($token, $userId)
     {
-        $user = RegistredUser::find($userId);
-
+        $user = $this->dbClient->findUserById($userId);
         if (!$user) {
             throw new Exception("El usuario especificado no existe.", ErrorCodes::TIMELINE_404);
         }
-
         $followedStreamers = json_decode($user->followedStreamers);
-
         if (empty($followedStreamers)) {
             return [];
         }
-
-        $clientId = ENV('CLIENT_ID');
-
         $streams = [];
-
         foreach (array_chunk($followedStreamers, 100) as $chunk) {
-            $response = Http::withHeaders([
-                'Client-Id' => $clientId,
-                'Authorization' => "Bearer $token",
-            ])->get('https://api.twitch.tv/helix/streams', [
-                'user_id' => $chunk,
-            ]);
-
-            if ($response->failed()) {
-                throw new Exception("Error al obtener los streams de Twitch.", ErrorCodes::TIMELINE_500);
-            }
-
-            $data = $response->json('data');
+            $data = $this->apiClient->getStreamsFromStreamer($token, $chunk);
             foreach ($data as $stream) {
                 $streams[] = [
                     'streamerId' => $stream['user_id'],
@@ -97,11 +70,9 @@ class UserDataProvider
                 ];
             }
         }
-
         usort($streams, function ($first, $second) {
             return strtotime($second['startedAt']) - strtotime($first['startedAt']);
         });
-
         return array_slice($streams, 0, 5);
     }
     /**
@@ -110,47 +81,33 @@ class UserDataProvider
     public function followStreamer($userId, $streamerId)
     {
         $user = $this->dbClient->findUserById($userId);
-
         if (!$user) {
             throw new Exception("El usuario especificado no existe.", ErrorCodes::USERS_404);
         }
-
         $followedStreamers = json_decode($user['followedStreamers'], true) ?? [];
-
         if (in_array($streamerId, $followedStreamers)) {
             throw new Exception("El usuario ya está siguiendo al streamer.", ErrorCodes::FOLLOW_409);
         }
-
         $followedStreamers[] = $streamerId;
-        $user['followedStreamers'] = json_encode($followedStreamers, JSON_UNESCAPED_SLASHES);
-
-        $this->dbClient->updateUserFollowedStreamers($userId, $user['followedStreamers']);
-        return $user;
+        return $this->dbClient->updateUserFollowedStreamers($user, json_encode($followedStreamers, JSON_UNESCAPED_SLASHES));
     }
 
+    /**
+     * @throws Exception
+     */
     public function unfollowStreamer($userId, $streamerId)
     {
         $user = $this->dbClient->findUserById($userId);
-
         if (!$user) {
             throw new Exception("El usuario especificado no existe.", ErrorCodes::USERS_404);
         }
-
         $followedStreamers = json_decode($user['followedStreamers'], true) ?? [];
-
         if (!in_array($streamerId, $followedStreamers)) {
             throw new Exception("El usuario no está siguiendo al streamer", 500);
         }
-
         $followedStreamers = array_values(array_filter($followedStreamers, function ($followedId) use ($streamerId) {
             return $followedId !== $streamerId;
         }));
-
-        $user['followedStreamers'] = json_encode($followedStreamers, JSON_UNESCAPED_SLASHES);
-
-        $this->dbClient->updateUserFollowedStreamers($userId, $user['followedStreamers']);
-        return $user;
+        return $this->dbClient->updateUserFollowedStreamers($user, json_encode($followedStreamers, JSON_UNESCAPED_SLASHES));
     }
-
-
 }
